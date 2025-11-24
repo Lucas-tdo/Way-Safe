1e21400e464efe60fa286dfd1e1230f00b1ba52c
Rebeca <rebeca.ferreira@sptech.school>
2025-11-24 02:05:03 -0300

    lógica inicial de listagem de usuários ADM e marcação de comentários para uso e aplicação de informa'ões na página de cadastro

---
+++ Projeto/src/routes/adm_waysafe_rota.js
@@ -0,0 +1,13 @@
+// copiado de concessionaria rota porque tem o mesmo objetivo. Listagem.
+var express = require("express");
+var router = express.Router();
+
+// chama o controller (próximo a ser criado)
+var TelaAdmWaysafeController = require("../controllers/tela_adm_waysafe_controller");
+
+// na controller então vai ter a função para pegar os usuários adm.
+router.get("/pegarFuncAdm",function(req,res){
+  TelaAdmWaysafeController.pegarFuncAdm(req,res)
+})
+
+module.exports = router;
